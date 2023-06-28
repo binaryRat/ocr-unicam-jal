fce4cb6efa8942b3ae8cf1d31e665c44a38394df
Julian <julian.marzoli@studenti.unicam.it>
2023-06-28 17:45:35 +0200

    Risoluzione bug

--- project/final-script/main.py
+++ project/final-script/main.py
@@ -19,26 +19,31 @@ ocr_technique = "none"
 
 if __name__ == '__main__':
     images = utils.load_images(input_dir)
-    path = None
+    den_path = None
     # denoising
     if args.tresholding:
         denoising_technique = "adaptive tresholding"
         denoised = denoising.adaptive_treshold(images)
-        path = os.path.join(output_dir, "adaptive-tresholding-results")
+        den_path = os.path.join(output_dir, "adaptive-tresholding-results")
     if args.edgedetection:
         denoising_technique = "edge detection"
         denoised = denoising.edge_detection(images)
         utils.save_images(denoised, output_dir + "/denoised/edge_detection")
-        path = os.path.join(output_dir, "edge-detection-results")
-    if not os.path.exists(path) and path is not None:
-        os.mkdir(path)
-        utils.save_images(denoised, path)
+        den_path = os.path.join(output_dir, "edge-detection-results")
+    if not os.path.exists(den_path) and den_path is not None:
+        os.mkdir(den_path)
+    utils.save_images(denoised, den_path)
+
     # ocr
     if args.ocr:
+        transcription_path = os.path.join(output_dir, "ocr-transcriptions")
+        if not os.path.exists(transcription_path):
+            os.mkdir(transcription_path)
         ocr_technique = "easy-ocr whit retrained model"
         for img in images:
             transcriptions.append(ocr.img_to_text(img))
-        utils.save_ocr_result(transcriptions, "output/transcriptions", True)
+        utils.save_ocr_result(transcriptions, transcription_path, True)
+
     print("Input dir: " + input_dir)
     print("Output dir: " + output_dir)
     print("Denoising: " + denoising_technique, ",", "Ocr: " + ocr_technique)
